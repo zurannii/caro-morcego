de905998c5bc20e3c64de9dc863f45c144867f1d
zurannii <zuranmrs2@gmail.com>
2025-06-12 08:14:30 -0300

    initial commit

---
+++ src/app/app.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  imports: [],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected title = 'meu-morcego';
+}
